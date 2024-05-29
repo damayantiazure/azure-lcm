0f8f795005e71f0072aae917e91d10a2c1ceed13
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-05-29 09:00:41 +0000

    Updated execute-job.sh

--- infra/execute-job.sh
+++ infra/execute-job.sh
@@ -48,6 +48,6 @@ az container create \
     AZURE_DEVOPS_USE_MANAGED_IDENTITY=false \
     AZURE_DEVOPS_USE_SERVICE_PRINCIPAL=false \
     AZURE_DEVOPS_PAT="$AZURE_DEVOPS_PAT" \
-    FEED_PROMPT_TEMPLATE_URI=https://gist.githubusercontent.com/damayantiazure/a670277b14d68149040de09bff560a4f/raw/eeeb4b59e1e8cb94c441d4cc5609d3d3eec68fe1/FeedPromptTemplate.txt" \
+    FEED_PROMPT_TEMPLATE_URI="https://gist.githubusercontent.com/damayantiazure/a670277b14d68149040de09bff560a4f/raw/eeeb4b59e1e8cb94c441d4cc5609d3d3eec68fe1/FeedPromptTemplate.txt" \
     FEED_WORKITEM_TEMPLATE_URI="https://gist.githubusercontent.com/damayantiazure/524ce416fb3e63b07b6ce0b869fd321b/raw/ee64445ef5e5deb3cfc9fcbd0e0219fde80642be/FeedWorkItemTemplateNew.json" \
     POLICY_WORKITEM_TEMPLATE_URI="https://gist.githubusercontent.com/damayantiazure/ba95444da66ced0a02b176522b8f740c/raw/beb47c21948da7d43ec122858ff9465ae003f019/PolicyWorkItemTemplateNew.json" 
